d131fca14a151ec4a9ae81356915cf3fc7a983a3
Marc-Antoine Vézina <marcant.vezina@gmail.com>
2018-02-08 11:26:36 -0500

    added test class and dependencies

---
+++ SquidSquAds/src/test/java/com/squidsquads/unit/service/campaign/CampaignServiceTest.java
@@ -0,0 +1,56 @@
+package com.squidsquads.unit.service.campaign;
+
+import com.squidsquads.form.campaign.request.CampaignCreateUpdateRequest;
+import com.squidsquads.model.campaign.Campaign;
+import com.squidsquads.repository.campaign.CampaignRepository;
+import com.squidsquads.service.campaign.CampaignService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.*;
+
+@ActiveProfiles("test")
+@RunWith(SpringRunner.class)
+public class CampaignServiceTest {
+
+    @Autowired
+    CampaignService campaignService;
+
+    @Test
+    public void testCampaignCreation(){
+        CampaignRepository repository = mock(CampaignRepository.class);
+        Campaign testCampaign = new Campaign(
+                null,
+                "testCampaign",
+                "https://URL_TO_IMG_HOST",
+                "https://URL_TO_IMG_HOST",
+                "https://URL_TO_IMG_HOST",
+                "http://www.google.ca",
+                new Date(),
+                new Date(),
+                800,
+                new Long[]{Long.valueOf(1), Long.valueOf(2), Long.valueOf(3)}
+        );
+
+        when(repository.save(testCampaign)).thenReturn(new Campaign(
+                Long.valueOf(1),
+                "testCampaign",
+                "https://URL_TO_IMG_HOST",
+                "https://URL_TO_IMG_HOST",
+                "https://URL_TO_IMG_HOST",
+                "http://www.google.ca",
+                new Date(),
+                new Date(),
+                800,
+                new Long[]{Long.valueOf(1), Long.valueOf(2), Long.valueOf(3)})
+        );
+
+        campaignService.addCampaign(new CampaignCreateUpdateRequest());
+        // TODO complete.
+    }
+}
