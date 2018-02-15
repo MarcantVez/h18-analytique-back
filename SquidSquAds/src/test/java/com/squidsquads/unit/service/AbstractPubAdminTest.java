2c04aa6c5078b5207a7fc053ba22540d65944161
Dulce Cayetano <dulcecayetanita@hotmail.com>
2018-02-15 14:30:27 -0500

    Test unitaires pour les profils

--- SquidSquAds/src/test/java/com/squidsquads/unit/service/AbstractPubAdminTest.java
+++ SquidSquAds/src/test/java/com/squidsquads/unit/service/AbstractPubAdminTest.java
@@ -1,5 +1,48 @@
 package com.squidsquads.unit.service;
 
+import com.squidsquads.model.account.Account;
+import com.squidsquads.model.account.AdminType;
+import com.squidsquads.model.account.WebSiteAdmin;
+import com.squidsquads.repository.account.AccountRepository;
+import com.squidsquads.repository.account.WebSiteAdminRepository;
+import com.squidsquads.service.account.AccountService;
+import com.squidsquads.service.account.WebSiteAdminService;
+import org.junit.BeforeClass;
+
+import java.util.Date;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
 public class AbstractPubAdminTest {
+
+    private static AccountService accountService;
+    private static WebSiteAdminService webSiteAdminService;
+
+    private static AccountRepository accountRepository;
+    private static WebSiteAdminRepository webSiteAdminRepository;
+
+    private static Account account;
+    private static WebSiteAdmin webSiteAdmin;
+
+    @BeforeClass
+    public static void init() {
+
+        //TODO create the account that creates the profiles
+
+        // Instance des services
+
+        // Dummy account
+
+        // Dummy web admin
+
+        // Mock account repositery
+
+        // Mock web site admin repositery
+
+        // Assigner les mocks aux instances des services
+
+    }
+
 
 }
