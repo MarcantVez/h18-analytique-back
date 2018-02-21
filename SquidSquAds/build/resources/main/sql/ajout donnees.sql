a5cdfd2fca90cf3b046f08f34a963d74426990e6
Dulce Cayetano <dulcecayetanita@hotmail.com>
2018-02-21 17:59:54 -0500

    added resources

---
+++ SquidSquAds/build/resources/main/sql/ajout donnees.sql
@@ -0,0 +1,154 @@
+--compteUtilisateur
+INSERT INTO compteUtilisateur VALUES
+  (DEFAULT,'WEB','adminWeb@test.com','mot_de_passe_en_clair','111AAA',NOW());
+
+
+INSERT INTO SiteWebAdmin VALUES
+  (DEFAULT,(select max(numero_compte) from compteUtilisateur),'https://www.siteWebA.com');
+
+INSERT INTO InfoDeSuivi VALUES
+  (DEFAULT,
+    (select max(numero_compte) from compteUtilisateur),
+    'e908f5b5ec9dedf0cb5bd964e1206f55',
+    'https://www.siteWebA.com/produit/5',
+    'https://www.siteWebA.com/produit/',
+    '192.168.0.1',
+    '2001:0db8:0000:85a3:0000:0000:ac1f:8001',
+    '1920x1080',
+    'fr-CA',
+    '00:00:20',
+    '2018-02-07 18:00'
+  );
+
+INSERT INTO AgentUtilisateur VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_InfoDeSuivi) FROM InfoDeSuivi),
+   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+   '63.0.3239.132',
+   'Windows NT 10.0',
+   'Chrome/63.0.3239.132 Safari/537.36',
+   'Win64',
+   'x64',
+   '',
+   '2018-02-07 18:00'
+  );
+
+INSERT INTO TypeNavigateur VALUES
+  (DEFAULT,
+   'Chrome'
+  );
+
+INSERT INTO TypeNavigateur_Agent VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_AgentUtilisateur) FROM AgentUtilisateur),
+   (SELECT MAX(numero_TypeNavigateur) FROM TypeNavigateur)
+  );
+
+INSERT INTO Banniere VALUES
+  (DEFAULT,
+   (select max(numero_compte) from compteUtilisateur),
+   'FFF000'
+  );
+
+INSERT INTO Orientation VALUES
+  (DEFAULT,
+   'Horizontal'
+  );
+
+INSERT INTO Orientation VALUES
+  (DEFAULT,
+   'Vertical'
+  );
+
+INSERT INTO Orientation VALUES
+  (DEFAULT,
+   'Mobile'
+  );
+
+INSERT INTO Orientation_Banniere VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_banniere) FROM Banniere),
+   (SELECT MAX(numero_orientation) FROM Orientation)
+  );
+
+INSERT INTO Categorie VALUES
+  (DEFAULT,
+   'Une publicitée qui a été vue');
+
+INSERT INTO Categorie VALUES
+  (DEFAULT,
+   'Une publicitée qui a été vue par un profil');
+
+INSERT INTO Categorie VALUES
+  (DEFAULT,
+   'Une publicitée qui a été cliquée');
+
+INSERT INTO Categorie VALUES
+  (DEFAULT,
+   'Une publicitée qui a été cliquée par un profil');
+
+INSERT INTO Visite VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_banniere) FROM Banniere),
+   '2018-02-07 18:00'
+  );
+
+INSERT INTO Categorie_Visite VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_categorie) FROM Categorie),
+   (SELECT MAX(numero_visite) FROM Visite)
+  );
+
+INSERT INTO Redevance VALUES
+  (DEFAULT,
+   (select max(numero_compte) from compteUtilisateur),
+   (SELECT MAX(numero_visite) FROM Visite)
+  );
+
+INSERT INTO Paiement VALUES
+  (DEFAULT,
+   (select max(numero_compte) from compteUtilisateur),
+   25,
+   '2018-02-07 18:00'
+  );
+
+INSERT INTO Campagne VALUES
+  (DEFAULT,
+    (select max(numero_compte) from compteUtilisateur),
+    'Campagne Alpha',
+    '2018-02-07 18:00',
+    'horizontal.jpg',
+    'vertical.jpg',
+    'mobile.jpg',
+    'https://www.microsoft.com/fr-ca/hololens',
+    '2018-02-08 18:00',
+    '2020-02-08 18:00',
+    200000
+  );
+
+INSERT INTO ProfilDUtilisateur VALUES
+  (DEFAULT,
+   (select max(numero_compte) from compteUtilisateur),
+   'name profil',
+   'description profil',
+   '2018-02-07 18:00'
+  );
+
+INSERT INTO Campagne_ProfilDutilisateur VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_campagne) FROM Campagne),
+   (SELECT MAX(numero_profildutilisateur) FROM ProfilDUtilisateur)
+  );
+
+INSERT INTO Site VALUES
+  (DEFAULT,
+   (SELECT MAX(numero_profildutilisateur) FROM ProfilDUtilisateur),
+   'https://www.siteWebA.com/produitB'
+  );
+
+
+
+--Deuxieme compte
+INSERT INTO compteUtilisateur VALUES
+  (DEFAULT,'PUB','adminPub@test.com','!Q@W#E4r','111BBB',NOW());
+
---
+++ SquidSquAds/build/resources/main/sql/creation tables.sql
@@ -0,0 +1,172 @@
+-- database: squidsquads
+
+-- drop database squidsquads;
+
+
+
+
+--create database squidsquads
+--with
+--owner = postgres
+--encoding = 'utf8'
+--lc_collate = 'english_canada.1252'
+--lc_ctype = 'english_canada.1252'
+--tablespace = pg_default
+--connection limit = -1;
+
+
+create table compteutilisateur
+(
+    numero_compte		serial primary key,
+    type_admin			varchar(3),
+    courriel			varchar(50),
+    mot_de_passe		varchar(50),
+    no_compte_banque	varchar(50),
+    date_creation		timestamp
+);
+
+create table paiement
+(
+    numero_paiement		serial primary key,
+    numero_compte		integer not null,
+    montant				numeric(10, 2),
+    date_paiement		timestamp
+);
+
+create table campagne
+(
+    numero_campagne		serial primary key,
+    numero_compte		integer not null,
+    nom					varchar(50),
+    date_creation		timestamp,
+    image_hor			varchar(100),
+    image_ver			varchar(100),
+    image_mob			varchar(100),
+    url_de_redirection	varchar(100),
+    date_debut			timestamp,
+    date_fin			timestamp,
+    budget				numeric(10, 2)
+);
+
+create table profildutilisateur
+(
+    numero_profildutilisateur	serial primary key,
+    numero_compte				integer not null,
+    nom							varchar(50),
+    description					varchar(200),
+    date_creation				timestamp
+);
+
+create table campagne_profildutilisateur
+(
+    numero	serial primary key,
+    numero_campagne						integer not null,
+    numero_profildutilisateur			integer not null
+
+);
+
+create table site
+(
+    numero_site					serial primary key,
+    numero_profildutilisateur	integer not null,
+    url							varchar(150)
+);
+
+create table banniere
+(
+    numero_banniere		serial primary key,
+    numero_compte		integer not null,
+    id_banniere				varchar(30)
+);
+
+create table orientation
+(
+    numero_orientation	serial primary key,
+    nom					varchar(50)
+);
+
+create table orientation_banniere
+(
+    numero_orientation_banniere	serial primary key,
+    numero_orientation			integer not null,
+    numero_banniere				integer not null
+);
+
+create table visite
+(
+    numero_visite		serial primary key,
+    numero_banniere		integer not null,
+    date_heure			timestamp
+);
+
+create table categorie
+(
+    numero_categorie	serial primary key,
+    nom					varchar(50)
+);
+
+create table categorie_visite
+(
+    numero_categorie_visite		serial primary key,
+    numero_categorie			integer not null,
+    numero_visite				integer not null
+);
+
+create table redevance
+(
+    numero_redevance	serial primary key,
+    numero_compte		integer not null,
+    numero_visite		integer not null,
+    montant				numeric(10, 2),
+    date_creation		timestamp,
+    est_reclame			boolean
+);
+
+create table sitewebadmin
+(
+    numero_sitewebadmin	serial primary key,
+    numero_compte		integer not null,
+    url					varchar(150)
+);
+
+create table infodesuivi
+(
+    numero_infodesuivi		serial primary key,
+    numero_sitewebadmin		integer not null,
+    empreinte				varchar(100),
+    urlactuel				varchar(150),
+    urlprovenance			varchar(150),
+    adresse_ipv4			varchar(100),
+    adresse_ipv6			varchar(100),
+    taille_ecran			varchar(100),
+    langue					varchar(50),
+    tempsecoule				time,
+    date_heure				timestamp
+);
+
+create table agentutilisateur
+(
+    numero_agentutilisateur		serial primary key,
+    numero_infodesuivi			integer not null,
+    agentutilisateurbrut		varchar(250),
+    versionnavigateur			varchar(100),
+    systeme_operation			varchar(100),
+    information_navigateur		varchar(100),
+    plateforme					varchar(100),
+    information_plateforme		varchar(100),
+    extension_navigateur		varchar(100),
+    date_heure				timestamp
+);
+
+create table typenavigateur
+(
+    numero_typenavigateur	serial primary key,
+    nom						varchar(50)
+);
+
+create table typenavigateur_agent
+(
+    numero_typenavigateur_agent		serial primary key,
+    numero_typenavigateur			integer not null,
+    numero_agentutilisateur			integer not null
+);
