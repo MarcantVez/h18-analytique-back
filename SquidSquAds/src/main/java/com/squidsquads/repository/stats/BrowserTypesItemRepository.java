62cd282bd5b82cde0ae83374dcee3decc1898f68
Simon Cote <cote.simon@live.ca>
2018-03-07 20:01:20 -0500

    Type Integer pour le BrowserRepository

--- SquidSquAds/src/main/java/com/squidsquads/repository/stats/BrowserTypesItemRepository.java
+++ SquidSquAds/src/main/java/com/squidsquads/repository/stats/BrowserTypesItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface BrowserTypesItemRepository extends JpaRepository<BrowserTypesItem, Long> {
+public interface BrowserTypesItemRepository extends JpaRepository<BrowserTypesItem, Integer> {
 
     List<BrowserTypesItem> findAllByWebsiteIDOrderByRatioDesc(Integer websiteID);
 
