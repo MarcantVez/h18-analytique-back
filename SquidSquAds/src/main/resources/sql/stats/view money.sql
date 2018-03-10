02ef26d3d233caea59e33a24f3bbfad0ee82eb93
Sam <SamLefebvre@users.noreply.github.com>
2018-03-10 13:49:33 -0500

    mise à jour stats redevance

--- SquidSquAds/src/main/resources/sql/stats/view money.sql
+++ SquidSquAds/src/main/resources/sql/stats/view money.sql
@@ -1,18 +1,164 @@
+CREATE OR REPLACE VIEW vw_stat_royaltefee_day AS
+  SELECT
+    id_campagne,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 0
+          THEN 0.01
+        ELSE 0
+        END)
+      sum_vue,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 1
+          THEN 0.03
+        ELSE 0
+        END)
+      sum_cible,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 10
+          THEN 0.05
+        ELSE 0
+        END)
+      sum_clique,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 11
+          THEN 0.10
+        ELSE 0
+        END)
+      sum_cible_clique
 
-CREATE OR REPLACE VIEW vw_stat_money_24h AS
-select id_campagne,
-  SUM(
-  CASE (est_ciblee::int+est_cliquee::int*10)
-  WHEN 0 THEN 0.01
-  WHEN 1 THEN 0.03
-  WHEN 10 THEN 0.05
-  WHEN 11 THEN 0.10
-  END)
-  montant
+  FROM visite v
+    INNER JOIN banniere b ON v.id_banniere = b.id_banniere
+    INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
 
-from visite v
-  INNER JOIN banniere b ON v.id_banniere = b.id_banniere
-  INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
+  WHERE date_heure > CURRENT_DATE - INTERVAL '1 day'
+  GROUP BY id_campagne
 
-WHERE  date_heure > CURRENT_DATE - INTERVAL '1 day'
-GROUP BY id_campagne
+CREATE OR REPLACE VIEW vw_stat_royaltefee_week AS
+  SELECT
+    id_campagne,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 0
+          THEN 0.01
+        ELSE 0
+        END)
+      sum_vue,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 1
+          THEN 0.03
+        ELSE 0
+        END)
+      sum_cible,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 10
+          THEN 0.05
+        ELSE 0
+        END)
+      sum_clique,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 11
+          THEN 0.10
+        ELSE 0
+        END)
+      sum_cible_clique
+
+  FROM visite v
+    INNER JOIN banniere b ON v.id_banniere = b.id_banniere
+    INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
+
+  WHERE date_heure > CURRENT_DATE - INTERVAL '1 week'
+  GROUP BY id_campagne
+
+
+CREATE OR REPLACE VIEW vw_stat_royaltefee_month AS
+  SELECT
+    id_campagne,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 0
+          THEN 0.01
+        ELSE 0
+        END)
+      sum_vue,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 1
+          THEN 0.03
+        ELSE 0
+        END)
+      sum_cible,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 10
+          THEN 0.05
+        ELSE 0
+        END)
+      sum_clique,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 11
+          THEN 0.10
+        ELSE 0
+        END)
+      sum_cible_clique
+
+  FROM visite v
+    INNER JOIN banniere b ON v.id_banniere = b.id_banniere
+    INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
+
+  WHERE date_heure > CURRENT_DATE - INTERVAL '1 month'
+  GROUP BY id_campagne
+
+
+CREATE OR REPLACE VIEW vw_stat_royaltefee_year AS
+  SELECT
+    id_campagne,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 0
+          THEN 0.01
+        ELSE 0
+        END)
+      sum_vue,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 1
+          THEN 0.03
+        ELSE 0
+        END)
+      sum_cible,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 10
+          THEN 0.05
+        ELSE 0
+        END)
+      sum_clique,
+    SUM(
+        CASE (est_ciblee :: INT + est_cliquee :: INT * 10)
+        WHEN 11
+          THEN 0.10
+        ELSE 0
+        END)
+      sum_cible_clique
+
+  FROM visite v
+    INNER JOIN banniere b ON v.id_banniere = b.id_banniere
+    INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
+
+  WHERE date_heure > CURRENT_DATE - INTERVAL '1 year'
+  GROUP BY id_campagne
+
+
+
+
+
+
+
