072027637d15846d734cca1211a854df36a1d965
Marc-Antoine Vézina <marcant.vezina@gmail.com>
2018-03-09 23:48:03 -0500

    Switched targeted view for browsertypes

--- SquidSquAds/src/main/java/com/squidsquads/model/BrowserTypesItem.java
+++ SquidSquAds/src/main/java/com/squidsquads/model/BrowserTypesItem.java
@@ -5,41 +5,54 @@ import org.hibernate.annotations.Immutable;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "browsertypes_view")
+@Table(name = "vw_stat_browser")
 @Immutable
 public class BrowserTypesItem {
-
     @Id
-    @Column(name = "id")
+    @Column(name = "rowid")
     private Integer id;
+
+    @Column(name = "id_sitewebadmin")
+    private Integer websiteID;
 
     @Column(name = "navigateur")
     private String name;
 
-    @Column(name = "ratio")
-    private int ratio;
+    @Column(name = "count")
+    private Integer count;
 
-    @Column(name = "websiteid")
-    private Integer websiteID;
+    @Column(name = "ratio")
+    private float ratio;
+
+
 
     public BrowserTypesItem() {
     }
 
-    public BrowserTypesItem(String name, int ratio, Integer websiteID) {
-        this.name = name;
-        this.ratio = ratio;
+    public BrowserTypesItem(Integer websiteID, String name, Integer count, float ratio) {
         this.websiteID = websiteID;
+        this.name = name;
+        this.count = count;
+        this.ratio = ratio;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public Integer getWebsiteID() {
+        return websiteID;
     }
 
     public String getName() {
         return name;
     }
 
-    public int getRatio() {
-        return ratio;
+    public Integer getCount() {
+        return count;
     }
 
-    public Integer getWebsiteID() {
-        return websiteID;
+    public float getRatio() {
+        return ratio;
     }
 }
