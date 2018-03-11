dc9adaa510a8c7c9d1b282795d1734f1a722e21c
Marc-Antoine Vézina <marcant.vezina@gmail.com>
2018-03-11 16:44:00 -0400

    removed id_campagne, used boolean operations

--- SquidSquAds/src/main/resources/sql/stats/view royalty.sql
+++ SquidSquAds/src/main/resources/sql/stats/view royalty.sql
@@ -7,63 +7,59 @@ FROM
 SELECT
      'daily' period,
      id_compte,
-     id_campagne,
-     SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 0 THEN 0.01 ELSE 0 END ) sum_vue,
-     SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 1 THEN 0.03 ELSE 0 END ) sum_cible,
-     SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 10 THEN 0.05 ELSE 0 END ) sum_clique,
-     SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 11 THEN 0.10 ELSE 0 END ) sum_cible_clique
+    SUM(CASE (NOT est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.01 ELSE 0 END) sum_vue,
+    SUM(CASE (est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.03 ELSE 0 END) sum_cible,
+    SUM(CASE (NOT est_ciblee AND est_cliquee) WHEN TRUE THEN 0.05 ELSE 0 END) sum_clique,
+    SUM(CASE (est_ciblee AND est_cliquee) WHEN TRUE THEN 0.10 ELSE 0 END) sum_cible_clique
    FROM visite v
      INNER JOIN banniere b ON v.id_banniere = b.id_banniere
      INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
    WHERE date_heure > CURRENT_DATE - INTERVAL '1 day'
-   GROUP BY id_compte, id_campagne
+   GROUP BY id_compte
 
 UNION ALL
 
    SELECT
   'weekly' period,
   id_compte,
-  id_campagne,
-  SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 0 THEN 0.01 ELSE 0 END ) sum_vue,
-  SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 1 THEN 0.03 ELSE 0 END ) sum_cible,
-  SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 10 THEN 0.05 ELSE 0 END ) sum_clique,
-  SUM( CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 11 THEN 0.10 ELSE 0 END ) sum_cible_clique
+     SUM(CASE (NOT est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.01 ELSE 0 END) sum_vue,
+     SUM(CASE (est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.03 ELSE 0 END) sum_cible,
+     SUM(CASE (NOT est_ciblee AND est_cliquee) WHEN TRUE THEN 0.05 ELSE 0 END) sum_clique,
+     SUM(CASE (est_ciblee AND est_cliquee) WHEN TRUE THEN 0.10 ELSE 0 END) sum_cible_clique
   FROM visite v
   INNER JOIN banniere b ON v.id_banniere = b.id_banniere
   INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
   WHERE date_heure > CURRENT_DATE - INTERVAL '1 week'
-  GROUP BY id_compte, id_campagne
+  GROUP BY id_compte
 
 UNION ALL
 
   SELECT
     'monthly' period,
     id_compte,
-    id_campagne,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 0 THEN 0.01 ELSE 0 END) sum_vue,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 1 THEN 0.03 ELSE 0 END) sum_cible,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 10 THEN 0.05 ELSE 0 END) sum_clique,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 11 THEN 0.10 ELSE 0 END) sum_cible_clique
+    SUM(CASE (NOT est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.01 ELSE 0 END) sum_vue,
+    SUM(CASE (est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.03 ELSE 0 END) sum_cible,
+    SUM(CASE (NOT est_ciblee AND est_cliquee) WHEN TRUE THEN 0.05 ELSE 0 END) sum_clique,
+    SUM(CASE (est_ciblee AND est_cliquee) WHEN TRUE THEN 0.10 ELSE 0 END) sum_cible_clique
   FROM visite v
     INNER JOIN banniere b ON v.id_banniere = b.id_banniere
     INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
   WHERE date_heure > CURRENT_DATE - INTERVAL '1 month'
-  GROUP BY id_compte,id_campagne
+  GROUP BY id_compte
 
 UNION ALL
   SELECT
     'annually' period,
     id_compte,
-    id_campagne,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 0 THEN 0.01 ELSE 0 END) sum_vue,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 1 THEN 0.03 ELSE 0 END) sum_cible,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 10 THEN 0.05 ELSE 0 END) sum_clique,
-    SUM(CASE (est_ciblee :: INT + est_cliquee :: INT * 10) WHEN 11 THEN 0.10 ELSE 0 END) sum_cible_clique
+    SUM(CASE (NOT est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.01 ELSE 0 END) sum_vue,
+    SUM(CASE (est_ciblee AND NOT est_cliquee) WHEN TRUE THEN 0.03 ELSE 0 END) sum_cible,
+    SUM(CASE (NOT est_ciblee AND est_cliquee) WHEN TRUE THEN 0.05 ELSE 0 END) sum_clique,
+    SUM(CASE (est_ciblee AND est_cliquee) WHEN TRUE THEN 0.10 ELSE 0 END) sum_cible_clique
   FROM visite v
     INNER JOIN banniere b ON v.id_banniere = b.id_banniere
     INNER JOIN banniere_campagne bc ON b.id_banniere = bc.id_banniere
   WHERE date_heure > CURRENT_DATE - INTERVAL '1 year'
-  GROUP BY id_compte,id_campagne
+  GROUP BY id_compte
     ) UnAlias
 
 
