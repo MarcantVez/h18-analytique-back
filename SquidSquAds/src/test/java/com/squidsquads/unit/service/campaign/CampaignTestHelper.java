9057b71de3937217b55f8d1ed00c3a2b3dd375c1
Simon Cote <cote.simon@live.ca>
2018-04-05 12:31:58 -0400

    Retirer le budget des tests

--- SquidSquAds/src/test/java/com/squidsquads/unit/service/campaign/CampaignTestHelper.java
+++ SquidSquAds/src/test/java/com/squidsquads/unit/service/campaign/CampaignTestHelper.java
@@ -12,26 +12,21 @@ public class CampaignTestHelper extends ServiceTestHelper {
     ///////////////////////
 
     public CreateRequest getCreateRequest() {
-        return buildCreateRequest(CAMPAIGN_NAME, REDIRECT_URL, PROFILE_IDS, START_DATE, END_DATE, BUDGET, HOR_URL, VER_URL, MOB_URL);
+        return buildCreateRequest(CAMPAIGN_NAME, REDIRECT_URL, PROFILE_IDS, START_DATE, END_DATE, HOR_URL, VER_URL, MOB_URL);
     }
 
     public CreateRequest getCreateRequestWhereOneParameterIsNullOrEmpty() {
-        return buildCreateRequest(CAMPAIGN_NAME, " ", PROFILE_IDS, START_DATE, END_DATE, BUDGET, HOR_URL, VER_URL, MOB_URL);
-    }
-
-    public CreateRequest getCreateRequestWhereBudgetIsNegative() {
-        return buildCreateRequest(CAMPAIGN_NAME, REDIRECT_URL, PROFILE_IDS, START_DATE, END_DATE, BigDecimal.valueOf(-20), HOR_URL, VER_URL, MOB_URL);
+        return buildCreateRequest(CAMPAIGN_NAME, " ", PROFILE_IDS, START_DATE, END_DATE, HOR_URL, VER_URL, MOB_URL);
     }
 
     private CreateRequest buildCreateRequest(String name, String redurl, Integer[] ids, String start, String end,
-                                             BigDecimal budget, String hor, String ver, String mob) {
+                                             String hor, String ver, String mob) {
         CreateRequest request = new CreateRequest();
         request.setName(name);
         request.setRedirectUrl(redurl);
         request.setProfileIds(ids);
         request.setStartDate(start);
         request.setEndDate(end);
-        request.setBudget(budget);
         request.setImgHorizontal(hor);
         request.setImgVertical(ver);
         request.setImgMobile(mob);
